d7c235e5531141d78f26552ff1552081085e68ed
Axel Ancona Esselmann <axel@anconaesselmann.com>
2018-01-21 01:38:12 -0800

    Bugfix

--- LA/Classes/LinearAlgebra.swift
+++ LA/Classes/LinearAlgebra.swift
@@ -5,34 +5,34 @@
 import UIKit
 
 public struct LineaAlgebra {
-    static func add(a: CGPoint, b: CGPoint) -> CGPoint {
+    public static func add(a: CGPoint, b: CGPoint) -> CGPoint {
         return CGPoint(x: a.x + b.x, y: a.y + b.y)
     }
-    static func subtract(a: CGPoint, b: CGPoint) -> CGPoint {
+    public static func subtract(a: CGPoint, b: CGPoint) -> CGPoint {
         return CGPoint(x: a.x - b.x, y: a.y - b.y)
     }
-    static func scalarMult(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    public static func scalarMult(point: CGPoint, scalar: CGFloat) -> CGPoint {
         return CGPoint(x: point.x * scalar, y: point.y * scalar)
     }
-    static func scalarDiv(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    public static func scalarDiv(point: CGPoint, scalar: CGFloat) -> CGPoint {
         return CGPoint(x: point.x / scalar, y: point.y / scalar)
     }
-    static func scalarAdd(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    public static func scalarAdd(point: CGPoint, scalar: CGFloat) -> CGPoint {
         return CGPoint(x: point.x + scalar, y: point.y + scalar)
     }
-    static func lenght(point: CGPoint) -> CGFloat {
+    public static func lenght(point: CGPoint) -> CGFloat {
         return sqrt(point.x * point.x + point.y * point.y)
     }
-    static func unit(point: CGPoint) -> CGPoint {
+    public static func unit(point: CGPoint) -> CGPoint {
         let l = lenght(point: point)
         return scalarDiv(point: point, scalar: l)
     }
-    static func orthUnit(point: CGPoint) -> CGPoint {
+    public static func orthUnit(point: CGPoint) -> CGPoint {
         let orth = CGPoint(x: -point.y, y: point.x)
         return unit(point: orth)
     }
     
-    static func changeOfBasis(for point: CGPoint, newOrigin origin: CGPoint, pointOnXAxis xAxisPoint: CGPoint) -> CGPoint {
+    public static func changeOfBasis(for point: CGPoint, newOrigin origin: CGPoint, pointOnXAxis xAxisPoint: CGPoint) -> CGPoint {
         let distanceC = point.x
         let orthogonalDistance = point.y
         
@@ -43,3 +43,4 @@ public struct LineaAlgebra {
         return add(a: pointC, b: scalarMult(point: vuOrth, scalar: orthogonalDistance))
     }
 }
+
